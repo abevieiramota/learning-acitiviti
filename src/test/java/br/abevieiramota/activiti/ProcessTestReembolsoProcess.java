7ac5abb771a8b5d084f5fa82b61fe3e483b3d340
Abelardo Vieira Mota <abevieiramota@gmail.com>
2016-10-23 12:42:11 -0200

    cade o hello world?

---
+++ src/test/java/br/abevieiramota/activiti/ProcessTestReembolsoProcess.java
@@ -0,0 +1,31 @@
+package test.java.br.abevieiramota.activiti;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.test.ActivitiRule;
+import org.activiti.engine.test.Deployment;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ProcessTestReembolsoProcess {
+
+	@Rule
+	public ActivitiRule activitiRule = new ActivitiRule();
+
+	@Test
+	@Deployment(resources="requestRefund.bpmn")
+	public void startProcess() throws Exception {
+		RuntimeService runtimeService = activitiRule.getRuntimeService();
+		Map<String, Object> variableMap = new HashMap<String, Object>();
+		variableMap.put("name", "Activiti");
+		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("reembolsoProcess", variableMap);
+		assertNotNull(processInstance.getId());
+		System.out.println("id " + processInstance.getId() + " "
+				+ processInstance.getProcessDefinitionId());
+	}
+}
